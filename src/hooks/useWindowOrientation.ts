5a156c2785d60153711433a92176c00c1fc9a128
rgsk <guptarahul70322@gmail.com>
2022-09-15 09:06:01 +0530

    reason for useWindowOrientation

--- src/hooks/useWindowOrientation.ts
+++ src/hooks/useWindowOrientation.ts
@@ -5,7 +5,11 @@ const sleep = (millis: number) => {
     setTimeout(resolve, millis);
   });
 };
-
+// primary value of this hook is that lets say,
+// chrome browser has better or new way or determining window orientation
+// while safari uses the old method, we can detect the browser and
+// set orientation accordingly and we provide a clean interface
+// for components to use the orientation, without changing code everywhere
 const useWindowOrientation = () => {
   const [orientation, setOrientation] = useState<
     'portrait' | 'landscape-primary' | 'landscape-secondary'
